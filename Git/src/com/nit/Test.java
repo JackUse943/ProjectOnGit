5751ed30d3f40ab2c83ebcec2186ffb38e47ce51
rjsur <rjsur@ASUS>
2024-11-10 01:06:19 +0530

    Arithmetic Operation

--- Git/src/com/nit/Test.java
+++ Git/src/com/nit/Test.java
@@ -1,5 +1,14 @@
 package com.nit;
 
-public class Test {
+public class Test 
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		int c=b/a;
+		System.out.println("Your result is "+c);
+		
+	}
 
 }
