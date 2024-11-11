7828171ab93fc769d26a0cf6a1f283d3ae012409
Abhishek Gupta <abhisheksatna001@gmail.com>
2024-11-11 17:35:54 +0530

    updated code

--- Git/src/com/nit/Test.java
+++ Git/src/com/nit/Test.java
@@ -1,22 +1,19 @@
 package com.nit;
 
-public class Test 
-{
-	public static void main(String[] args)
-	{
-		int a=10;
-		int b=20;
-		int c=b/a;
-		System.out.println("Your result is "+c);
-		System.out.println("Your result is "+(a+b));
+public class Test {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = b / a;
+		System.out.println("Your result is " + c);
+		System.out.println("Your result is " + (a + b));
 		System.out.println("==============================");
-		System.out.println("Abhishek is very much Chutiya Man");
-		System.out.println("Abhishek is very much Chutiya Man");
-		
-		
-		
-		
-		
+
+		for (int i = 0; i < 500; i++) {
+			System.out.println("Suraj is very much Chutiya Man");
+			System.out.println("Suraj is very much Chutiya Man");
+		}
+
 	}
 
 }
